831ab129df3f9368048bdb96a0b9d1f75d0ec55c
maxre24 <cyphe371@gmail.com>
2023-11-09 16:37:03 -0800

    Update client.js

--- client.js
+++ client.js
@@ -1,46 +1,37 @@
 const canvas = document.getElementById('gameCanvas');
-const cursor = new Cursor(canvas);
-const opponentCursor = new OpponentCursor(canvas);
+const ctx = canvas.getContext('2d');
 
-// Hide the actual cursor
-canvas.style.cursor = 'none';
-const socket = io('https://cursordash.schiytu37.repl.co/');
+const socket = io('https://cursordash.schiytu37.repl.co'); // Replace with your Replit server URL
 
-// Update cursor position and direction based on mouse movement
-canvas.addEventListener('mousemove', (event) => {
-    const rect = canvas.getBoundingClientRect();
-    const mouseX = event.clientX - rect.left;
-    const mouseY = event.clientY - rect.top;
+const playerCursors = {};
 
-    // Emit the cursor position and angle to the server using socket.emit()
-    socket.emit('cursorPosition', { x: mouseX, y: mouseY, angle: cursor.angle });
-
-    cursor.update(mouseX, mouseY);
-});
-
-// Listen for opponent cursor data from the socket and update opponent's cursor
 socket.on('opponentCursorPosition', (data) => {
-    opponentCursor.update(data);
+    // Update opponent cursor positions
+    playerCursors[data.id] = data;
 });
 
-// Main game loop
-function gameLoop() {
-    // Clear canvas
-    cursor.ctx.clearRect(0, 0, cursor.canvas.width, cursor.canvas.height);
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Draw player's cursor
-    cursor.draw();
+    // Render player cursors
+    Object.values(playerCursors).forEach(cursor => {
+        ctx.fillStyle = cursor.id === socket.id ? 'blue' : 'red'; // Local player is blue, opponents are red (change colors as needed)
+        ctx.beginPath();
+        ctx.arc(cursor.x, cursor.y, 10, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.closePath();
+    });
 
-    // Draw opponent's cursor
-    opponentCursor.draw();
-
-    // Request animation frame for the next loop iteration
-    requestAnimationFrame(gameLoop);
+    requestAnimationFrame(update);
 }
 
-// Start the game loop after the cursor images are loaded
-cursor.image.onload = () => {
-    opponentCursor.image.onload = () => {
-        gameLoop();
-    };
-};
+canvas.addEventListener('mousemove', (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    // Send cursor position to the server
+    socket.emit('cursorPosition', { id: socket.id, x, y });
+});
+
+update();
